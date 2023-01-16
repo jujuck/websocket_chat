f984c9b3594bdc7e603745725d0cb6c1209be57f
Julien RICHARD <julien.y.richard@gmail.com>
2023-01-16 09:59:15 +0100

    Integration of the client interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,47 @@
-import Home from "./pages/Home";
-
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+  const [nickName, setNickName] = useState("");
+  const [newMessageText, setNewMessageText] = useState("");
+  // const [socket, setSocket] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessageList([]);
+  };
+
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <h2>Messages</h2>
+      {messageList.map((message) => {
+        return (
+          <div key={message.id}>
+            {message.author} : {message.text}
+          </div>
+        );
+      })}
+
+      <form onSubmit={handleSubmit}>
+        <h2>New Message</h2>
+        <input
+          type="text"
+          name="author"
+          placeholder="nickname"
+          value={nickName}
+          required
+          onChange={(e) => setNickName(e.target.value)}
+        />
+        <input
+          type="text"
+          name="messageContent"
+          placeholder="message"
+          value={newMessageText}
+          required
+          onChange={(e) => setNewMessageText(e.target.value)}
+        />
+        <input type="submit" value="send" />
+      </form>
     </div>
   );
 }
